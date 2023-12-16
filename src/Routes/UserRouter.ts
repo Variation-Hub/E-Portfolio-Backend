7c0de009cb7902dace7720dfff2d8c1c3a3f2192
jeel <jeeltadhani3@gmail.com>
2023-12-16 23:21:54 +0530

    change in user api req

--- src/Routes/UserRouter.ts
+++ src/Routes/UserRouter.ts
@@ -11,9 +11,9 @@ const Controller = new UserController();
 
 userRoutes.post("/create", verifyTokenMiddleware, trimMiddleware, Controller.CreateUser);
 userRoutes.get("/get", verifyTokenMiddleware, Controller.GetUser);
-userRoutes.patch("/update/:id", checkRoleTokenMiddleware, Controller.UpdateUser);
-userRoutes.post("/login", Controller.LoginUser);
-userRoutes.post("/updatepassword", verifyTokenMiddleware, Controller.PasswordChangeUser);
+userRoutes.patch("/update/:id", checkRoleTokenMiddleware, trimMiddleware, Controller.UpdateUser);
+userRoutes.post("/login", trimMiddleware, Controller.LoginUser);
+userRoutes.post("/updatepassword", verifyTokenMiddleware, trimMiddleware, Controller.PasswordChangeUser);
 userRoutes.delete("/delete", verifyTokenMiddleware, Controller.DeleteUser);
 userRoutes.get("/list", verifyAdminTokenMiddleware, paginationMiddleware, Controller.GetUserList);
 
