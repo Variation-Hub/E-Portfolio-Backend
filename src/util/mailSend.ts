4f78dc904be7f2422a8b010c753409522a1d39df
jeel <jeeltadhani3@gmail.com>
2024-07-29 18:58:53 +0530

    sss

--- src/util/mailSend.ts
+++ src/util/mailSend.ts
@@ -1,80 +1,85 @@
 import { SendEmailTemplet } from "./nodemailer";
 
 export const sendPasswordByEmail = async (email: string, password: any): Promise<boolean> => {
-    const html = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            body {
-                font-family: 'Arial', sans-serif;
-                background-color: #f0f0f0;
-                margin: 0;
-                padding: 0;
-                text-align: center;
-            }
-    
-            .container {
-                max-width: 600px;
-                margin: 20px auto;
-                background-color: #ffffff;
-                padding: 20px;
-                border-radius: 10px;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            }
-    
-            .logo {
-                max-width: 150px;
-                height: auto;
-                margin-bottom: 20px;
-            }
-    
-            .title {
-                font-size: 24px;
-                font-weight: bold;
-                margin-bottom: 10px;
-                color: #333;
-            }
-    
-            .message {
-                font-size: 16px;
-                margin-bottom: 20px;
-                color: #555;
-            }
-    
-            .password {
-                font-weight: bold;
-                font-size: 20px;
-                color: #3498db;
-            }
-    
-            .footer {
-                font-size: 16px;
-                color: #777;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <!-- Add your logo here -->
-            <img class="logo" src="https://jeel1.s3.ap-south-1.amazonaws.com/logo/logo.svg" alt="Locker Logo">
-    
-            <div class="title">Welcome to Locker</div>
-            <div class="message">
-                <p>Congratulations! Your account has been successfully created.</p>
+    try {
+        const html = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                body {
+                    font-family: 'Arial', sans-serif;
+                    background-color: #f0f0f0;
+                    margin: 0;
+                    padding: 0;
+                    text-align: center;
+                }
+        
+                .container {
+                    max-width: 600px;
+                    margin: 20px auto;
+                    background-color: #ffffff;
+                    padding: 20px;
+                    border-radius: 10px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                }
+        
+                .logo {
+                    max-width: 150px;
+                    height: auto;
+                    margin-bottom: 20px;
+                }
+        
+                .title {
+                    font-size: 24px;
+                    font-weight: bold;
+                    margin-bottom: 10px;
+                    color: #333;
+                }
+        
+                .message {
+                    font-size: 16px;
+                    margin-bottom: 20px;
+                    color: #555;
+                }
+        
+                .password {
+                    font-weight: bold;
+                    font-size: 20px;
+                    color: #3498db;
+                }
+        
+                .footer {
+                    font-size: 16px;
+                    color: #777;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <!-- Add your logo here -->
+                <img class="logo" src="https://jeel1.s3.ap-south-1.amazonaws.com/logo/logo.svg" alt="Locker Logo">
+        
+                <div class="title">Welcome to Locker</div>
+                <div class="message">
+                    <p>Congratulations! Your account has been successfully created.</p>
+                </div>
+                <div class="password">Your temporary password is: <strong>${password}</strong></div>
+                <div class="footer">
+                    <p>Thank you for using Locker.</p>
+                </div>
             </div>
-            <div class="password">Your temporary password is: <strong>${password}</strong></div>
-            <div class="footer">
-                <p>Thank you for using Locker.</p>
-            </div>
-        </div>
-    </body>
-    </html>
-    `
+        </body>
+        </html>
+        `
 
-    const responce = await SendEmailTemplet(email, "Welcome", null, html)
-    return true
+        const responce = await SendEmailTemplet(email, "Welcome", null, html)
+        return true
+    } catch (error) {
+        console.log(error)
+        return true
+    }
 }
 
 const generateOTP = (): string => {
